7e60f04a5aaf32e4c32f975b8de42d9b62b732e1
Tadeu <tazzeus@gmail.com>
2025-01-11 00:32:20 -0300

    [ 2 aulas] - Definindo a cor do Fundo do app e Criando o Header da Index

--- links/src/app/index/index.tsx
+++ links/src/app/index/index.tsx
@@ -1,12 +1,19 @@
-import { Text, View } from 'react-native'
+import { View, Image, TouchableOpacity } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'
+
 import { styles } from './styles'
+import { colors } from '@/styles/colors'
 
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello</Text>
-      <Text style={styles.title}>React Native!</Text>
+      <View style={styles.header}> 
+        <Image source={require("@/assets/logo.png")} style={styles.logo} />
+        <TouchableOpacity>
+        <MaterialIcons name="add" size={32} color={colors.green[300]} />      
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }

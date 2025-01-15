105253d992557ef32556f9c1ca973bed1b28089a
Tadeu <tazzeus@gmail.com>
2025-01-14 21:54:05 -0300

    [3 aulas] - Criando o componente Category, Utilizando Propriedades no Componente e Aplicando Variação de Estilos

--- links/src/app/index/index.tsx
+++ links/src/app/index/index.tsx
@@ -4,6 +4,8 @@ import { MaterialIcons } from '@expo/vector-icons'
 import { styles } from './styles'
 import { colors } from '@/styles/colors'
 
+import { Category } from '@/components/category'
+
 
 export default function Index() {
   return (
@@ -14,6 +16,11 @@ export default function Index() {
         <MaterialIcons name="add" size={32} color={colors.green[300]} />      
         </TouchableOpacity>
       </View>
+
+      <Category name="Projetos" icon="code" isSelected />
+      <Category name="Site" icon="language" isSelected={false} />
+      <Category name="Vídeo" icon="movie" isSelected={false} />
+
     </View>
   )
 }
--- links/src/app/index/styles.ts
+++ links/src/app/index/styles.ts
@@ -5,7 +5,7 @@ import { MaterialIcons } from "@expo/vector-icons"
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 62,
+    paddingTop: 62,  
   },
   
   title: {

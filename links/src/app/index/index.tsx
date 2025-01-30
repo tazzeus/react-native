b2fbe4a9932fe82f30b472cc81c04eb853ef468c
Tadeu <tazzeus@gmail.comm>
2025-01-30 17:53:36 -0300

    [2 aulas] - Utilizando Modal e Criando componente Option

--- links/src/app/index/index.tsx
+++ links/src/app/index/index.tsx
@@ -1,8 +1,9 @@
-import { View, Image, TouchableOpacity, FlatList } from 'react-native'
+import { View, Image, TouchableOpacity, FlatList, Modal, Text } from 'react-native'
 import { MaterialIcons } from '@expo/vector-icons'
 
 import { Link } from '@/components/link'
 import { Categories } from '@/components/categories'
+import { Option } from '@/components/option'
 import { colors } from '@/styles/colors'
 import { styles } from './styles'
 
@@ -35,6 +36,29 @@ export default function Index() {
         contentContainerStyle={styles.linksContent}
         showsVerticalScrollIndicator={false}
       />
+
+      <Modal transparent visible={true}>
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            
+            <View style={styles.modalheader}>
+              <Text style={styles.modalCategory}> Curso </Text>
+              <TouchableOpacity> 
+                <MaterialIcons name='close' size={20} color={colors.gray[400]}/>
+              </TouchableOpacity>
+            </View>
+
+            <Text style={styles.modalLinkName}> Rocketseat </Text>
+            <Text style={styles.modalUrl}> https://www.rocketseat.com.br </Text>
+
+            <View style={styles.modalFooter}>
+              <Option name="Excluir" icon="delete" variant="secondary" />
+              <Option name="Abrir" icon="language" />
+            </View>
+
+          </View>
+        </View>
+      </Modal>
 
     </View>
   )

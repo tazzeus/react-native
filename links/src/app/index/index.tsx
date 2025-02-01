c4d2f3821bcaeb7aa7646bffe025c2dd0516160e
Tadeu <tazzeus@gmail.comm>
2025-01-31 21:46:17 -0300

    [2 aulas] - Criando a Tela para Adicionar Novos Links e Navegando

--- links/src/app/index/index.tsx
+++ links/src/app/index/index.tsx
@@ -1,5 +1,6 @@
 import { View, Image, TouchableOpacity, FlatList, Modal, Text } from 'react-native'
 import { MaterialIcons } from '@expo/vector-icons'
+import { router } from 'expo-router'
 
 import { Link } from '@/components/link'
 import { Categories } from '@/components/categories'
@@ -13,7 +14,7 @@ export default function Index() {
     <View style={styles.container}>
       <View style={styles.header}>
         <Image source={require("@/assets/logo.png")} style={styles.logo} />
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => router.navigate('/add')}>
           <MaterialIcons name="add" size={32} color={colors.green[300]} />
         </TouchableOpacity>
       </View>
@@ -37,7 +38,7 @@ export default function Index() {
         showsVerticalScrollIndicator={false}
       />
 
-      <Modal transparent visible={true}>
+      <Modal transparent visible={false}>
         <View style={styles.modal}>
           <View style={styles.modalContent}>
             

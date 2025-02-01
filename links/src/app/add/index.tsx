3b590d8e8a5c1d99d194e629d2f2ac58e7cf5534
Tadeu <tazzeus@gmail.comm>
2025-02-01 19:44:23 -0300

    [2 Aulas] - Recuperando Valor do Input e Criando Estados

--- links/src/app/add/index.tsx
+++ links/src/app/add/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { View, Text, TouchableOpacity } from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
 import { router } from "expo-router"
@@ -10,6 +11,14 @@ import { Button } from "@/components/button"
 
 
 export default function Add() {
+    
+    const [name, setName] = useState("")
+    const [url, setUrl] = useState("")
+
+    function handleAdd(){
+        console.log({name, url})
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
@@ -24,13 +33,10 @@ export default function Add() {
             <Categories />
 
             <View style={styles.form}>
-                <Input placeholder="Nome" />
-                <Input placeholder="Url" />
-                <Button title="Adicionar" />
+                <Input placeholder="Nome" onChangeText={setName} autoCorrect={false} />
+                <Input placeholder="Url" onChangeText={setUrl} autoCorrect={false} />
+                <Button title="Adicionar" onPress={handleAdd} />
             </View>
-            
-            
-
         </View>
     )
 }
